8240e43e1e8f1d627ff93a6434313fb17525d10c
searsb <fbbryan@gmail.com>
2020-12-01 20:30:56 -0600

    Day 1 2019

--- 2019/Day1.py
+++ 2019/Day1.py
@@ -12,18 +12,26 @@ The Fuel Counter-Upper needs to know the total fuel requirement. To find it, ind
 
 What is the sum of the fuel requirements for all of the modules on your spacecraft?
 """
-ModuleSize =[125517,140694,65516,98562,75660,133603,114499,81732,119081,50911,96650,98330,145164,64851,67455,108208,102674,147581,112059,62456,132006,88738,72139,121074,103936,65149,82081,90168,134670,79142,83296,109983,60250,61982,136326,52980,79969,66851,77049,59720,73494,115708,109326,136399,72950,82041,105467,112321,125019,79213,107186,148340,112833,125646,112509,52396,59446,93967,73179,88725,98256,143303,57503,120314,147921,130856,95561,145857,54976,100605,77961,143120,84127,130389,131848,109542,119653,61660,124800,61498,149675,143906,120361,68328,104473,54279,119945,122511,109410,135350,112070,88822,149086,64594,118788,102569,61721,89170,83581,58722]
+my_file = open("2019/Day1Input.txt", "r")
+ModuleSize = my_file.read().splitlines()
+my_file.close()
+
 testsize = [14,12,1969,100756]
+#testsize = [1969]
 totalcalc = 0
+
+
 def SizeCalc(Size):
-    calc = (Size//3)-2
+    calc = (int(Size)//3)-2
     return calc
 for x in ModuleSize:
     calc = SizeCalc(x)
     totalcalc=calc+totalcalc
-
+##Part 1
+print('-'*25)
+print('Part 1')
 print (totalcalc)
-
+print('-'*25)
 """
 --- Part Two ---
 During the second Go / No Go poll, the Elf in charge of the Rocket Equation Double-Checker stops the launch sequence. Apparently, you forgot to include additional fuel for the fuel you just added.
@@ -39,16 +47,22 @@ What is the sum of the fuel requirements for all of the modules on your spacecra
 
 
 """
-ModuleSize =[125517,140694,65516,98562,75660,133603,114499,81732,119081,50911,96650,98330,145164,64851,67455,108208,102674,147581,112059,62456,132006,88738,72139,121074,103936,65149,82081,90168,134670,79142,83296,109983,60250,61982,136326,52980,79969,66851,77049,59720,73494,115708,109326,136399,72950,82041,105467,112321,125019,79213,107186,148340,112833,125646,112509,52396,59446,93967,73179,88725,98256,143303,57503,120314,147921,130856,95561,145857,54976,100605,77961,143120,84127,130389,131848,109542,119653,61660,124800,61498,149675,143906,120361,68328,104473,54279,119945,122511,109410,135350,112070,88822,149086,64594,118788,102569,61721,89170,83581,58722]
-testsize = [14,12,1969,100756]
-totalcalc = 0
+calc = 2
+
 def FuelCalc(Size):
-    calc = (Size//3)-2
-   
+    calc = (int(Size)//3)-2
     return calc
-
+totalcalc = 0
 for x in ModuleSize:
-    calc = SizeCalc(x)
-    totalcalc=calc+totalcalc
 
+    moduleFuel = FuelCalc(x)
+    TotalFuel = moduleFuel
+    while FuelCalc(moduleFuel) >= 0:
+       Fuel = FuelCalc(moduleFuel)
+       TotalFuel = TotalFuel + Fuel
+       moduleFuel = Fuel
+    totalcalc = TotalFuel + totalcalc
+   
+print('Part 2')
 print (totalcalc)
+print('-'*25)
